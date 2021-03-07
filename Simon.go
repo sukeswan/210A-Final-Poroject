4498d9b1bbb60e524224525a840b6a21a4995c0d
dolstewa <dolstewa@ucsc.edu>
2021-03-07 15:54:37 -0800

    type conversion finally

--- Simon.go
+++ Simon.go
@@ -3,47 +3,54 @@ package main
 import (
 	"fmt"
   "strconv"
-  "strings"
+  // "strings"
 )
 
 
-func hex_to_binary(input string, size string) string{
-  // bits := []int{}
-  bit_ui, err := strconv.ParseUint(input, 16, 64)
-  if err != nil {
-		return "wtf"
+func hex_to_binary(input string) []int{
+  var bits []int
+  bitString := ""
+  loop_range := len(input)/16
+
+  for i := 0; i < loop_range; i++ {
+    idx := i*16
+    bit_ui, _ := strconv.ParseUint(input[idx:idx+16], 16, 64)
+    bitString = bitString + fmt.Sprintf("%064b", bit_ui)
+  }
+  for _ , bit := range bitString {
+    b, _ := strconv.Atoi(string(bit))
+		bits = append(bits, b)
 	}
 
-  b_input := strings.Join([]string{"%0", size , "b"}, "")
-  //iterate through string every 16 digits
-  //convert and add to bit string
-  //for s in string slice append return int slice
-
-
-
-  // for i, c := range input{
-  //
-  // }
-  // for i := 0; i < 24; i++ {
-  //       bits = append([]int{val & 0x1}, bits...)
-  //       // or
-  //       // bits = append(bits, val & 0x1)
-  //       // depending on the order you want
-  //       val = val >> 1
-  //   }
-  bitString := fmt.Sprintf(b_input, bit_ui)
-  return bitString
-  // return bits
+  return bits
 }
 
 
 func main() {
   // plain_text := "74206e69206d6f6f6d69732061207369"
-  key := "1f1e1d1c1b1a191817161514131211100f0e0d0c0b0a09080706050403020100"
+  key := "1f1e1d1c1b1a191817161514131211100f0e0d0c0b0a09080706050403020100" //
   // ciphertext  := "8d2b5579afc8a3a03bf72a87efe7b868"
 	// z4 := []int{1,1,0,1,0,0,0,1,1,1,1,0,0,1,1,0,1,0,1,1,0,1,1,0,0,0,1,0,0,0,0,0,0,1,0,1,1,1,0,0,0,0,1,1,0,0,1,0,1,0,0,1,0,0,1,1,1,0,1,1,1,1}
-  binary_key := hex_to_binary(key,"256")
-  fmt.Printf(binary_key)
-  // s.constructor("E", slc)
-	// fmt.Printf("en %s, %s", s.en_de,s.key )
+  binary_key := hex_to_binary(key)
+  fmt.Printf("%v\n", binary_key)
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
